676ff148cb511c4787f6f03d29cd231138ece758
Edward Lu <yanlu1990@hotmail.co.uk>
2017-01-13 14:47:31 +0800

    abstruct basic breath action as a reusable function

--- fade_digital_led/fade_digital_led.ino
+++ fade_digital_led/fade_digital_led.ino
@@ -1,40 +1,48 @@
 //http://www.rngtng.com/2009/05/17/arduino-fade-and-pulse-a-led-with-just-using-a-digitalport/
 
 
-int ledPin = 13;
+int led1 = 2;
 unsigned int i = 0;
 boolean rise = true;
-int period = 1000;
+int breath_period = 1000;
+
+
+
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led1, OUTPUT);
 }
+
+
+
 void loop()
 {
-  if (i == period)
+  breath_led(led1, breath_period);
+}
+
+
+//breath the led through certain digital pin 
+void breath_led(int led_pin, int period){
+  if (i == breath_period)
   {
     i = 1;
     rise = !rise;
   }
   if (rise == false)
   {
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(led_pin, LOW);
     delayMicroseconds(i);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delayMicroseconds(period - i);
+    digitalWrite(led_pin, HIGH);
+    delayMicroseconds(breath_period - i);
     i = i + 1;
   }
   if (rise == true)
   {
     digitalWrite(13, LOW);
-    delayMicroseconds(period - i);
+    delayMicroseconds(breath_period - i);
     digitalWrite(13, HIGH);
     delayMicroseconds(i);
     i = i + 1;
-  }
-}
-
-void breath_led(int led_pin, period){
-  
+  }  
 }
 
