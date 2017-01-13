864ef904d14fd834f66ffebdf4800547cf083564
Edward Lu <yanlu1990@hotmail.co.uk>
2017-01-13 15:45:35 +0800

    initial test on leds array successed

--- fade_digital_led/fade_digital_led.ino
+++ fade_digital_led/fade_digital_led.ino
@@ -3,6 +3,8 @@
 
 int led1 = 2;
 int led2 = 3;
+int led_array[4] = {2,3,4,5};
+ 
 unsigned int i = 0;
 boolean rise = true;
 int breath_period = 1000;
@@ -11,18 +13,23 @@ int breath_period = 1000;
 
 void setup()
 {
-  pinMode(led1, OUTPUT);
+  Serial.begin(9600);
+  int j;
+  for (j=0; j < (sizeof(led_array)/sizeof(int)); j++){
+      Serial.println(led_array[j]);
+      pinMode(led_array[j], OUTPUT);
+    }  
 }
 
 
 void loop()
 {
-  breath_led(led1, led2, breath_period);
+  breath_led(led_array, breath_period);
 }
 
 
 //breath the led through certain digital pin 
-void breath_led(int led_pin1, int led_pin2,  int period){
+void breath_led(int leds[], int period){
   if (i == breath_period)
   {
     i = 1;
@@ -30,21 +37,27 @@ void breath_led(int led_pin1, int led_pin2,  int period){
   }
   if (rise == false)
   {
-    digitalWrite(led_pin1, LOW);
-    digitalWrite(led_pin2, LOW);
+    int j;
+    for (j=0; j < (sizeof(led_array)/sizeof(int)); j++){
+      digitalWrite(leds[j], LOW);
+    }
     delayMicroseconds(i);
-    digitalWrite(led_pin1, HIGH);
-    digitalWrite(led_pin2, HIGH);
+    for (j=0; j < (sizeof(led_array)/sizeof(int)); j++){
+      digitalWrite(leds[j], HIGH);
+    }
     delayMicroseconds(breath_period - i);
     i = i + 1;
   }
   if (rise == true)
   {
-    digitalWrite(led_pin1, LOW);
-    digitalWrite(led_pin2, LOW);
+    int j;
+    for (j=0; j < (sizeof(led_array)/sizeof(int)); j++){
+      digitalWrite(leds[j], LOW);
+    }
     delayMicroseconds(breath_period - i);
-    digitalWrite(led_pin1, HIGH);
-    digitalWrite(led_pin2, HIGH);
+    for (j=0; j < (sizeof(led_array)/sizeof(int)); j++){
+      digitalWrite(leds[j], HIGH);
+    }
     delayMicroseconds(i);
     i = i + 1;
   }  
