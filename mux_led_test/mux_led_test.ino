67eacdc256bea99a9ec9c5fcfcbbae1b79996bf2
Edward Lu <yanlu1990@hotmail.co.uk>
2017-01-14 15:21:36 +0800

    start making fade effects

--- mux_led_test/mux_led_test.ino
+++ mux_led_test/mux_led_test.ino
@@ -4,16 +4,32 @@
 //int s2 = 4;
 //int s3 = 5;
 enum { s0 = 2, s1 = 3, s2 = 4, s3 = 5};
-int SIG_pin = 9; 
+int SIG_pin = 9;
 
 
-int channels[] = {1,2,3,4,5};
+int channels[] = {1, 2, 3, 4};
+#define NUM_CHANNELS (sizeof(channels)/sizeof(int))
+
+
+
+//fade control
+int max_fade_intensity = 255;
+int min_fade_intensity = 40;
+int fade_intensity_decrement_step = 5; //ms
+int dimming_step = 30; //ms
+int lighting_interval = 1000; //ms
+int fades_interval = 1000; //ms
+// end of fade control
+
+
+
+
 
 void setup() {
-  pinMode(s0, OUTPUT); 
-  pinMode(s1, OUTPUT); 
-  pinMode(s2, OUTPUT); 
-  pinMode(s3, OUTPUT); 
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
 
   digitalWrite(s0, LOW);
   digitalWrite(s1, LOW);
@@ -22,57 +38,65 @@ void setup() {
 
   analogWrite(SIG_pin, 255);
   Serial.begin(9600);
-  Serial.println(sizeof(channels));
+
+
+
 }
 
 void loop() {
-  read_mul_mux(channels);
-//  Serial.println(sizeof(channels));
+
+  read_mux(channels, NUM_CHANNELS);
 }
 
 
-void read_mul_mux (int channels[]) {
-  Serial.println(sizeof(channels));
 
-  for (int i = 0; i<(sizeof(channels)/sizeof(int)); i++) {
-    read_mux(channels[i]);
+void read_mux(int channels[], int loop_size) {
+  for (int j = 0; j < loop_size; j++) {
+    int controlPin[] = {s0, s1, s2, s3};
+    int muxChannel[16][4] = {
+      {0, 0, 0, 0}, //channel 0
+      {1, 0, 0, 0}, //channel 1
+      {0, 1, 0, 0}, //channel 2
+      {1, 1, 0, 0}, //channel 3
+      {0, 0, 1, 0}, //channel 4
+      {1, 0, 1, 0}, //channel 5
+      {0, 1, 1, 0}, //channel 6
+      {1, 1, 1, 0}, //channel 7
+      {0, 0, 0, 1}, //channel 8
+      {1, 0, 0, 1}, //channel 9
+      {0, 1, 0, 1}, //channel 10
+      {1, 1, 0, 1}, //channel 11
+      {0, 0, 1, 1}, //channel 12
+      {1, 0, 1, 1}, //channel 13
+      {0, 1, 1, 1}, //channel 14
+      {1, 1, 1, 1} //channel 15
+    };
+    //loop through the 4 sig
+    for (int i = 0; i < 4; i ++) {
+      digitalWrite(controlPin[i], muxChannel[channels[j]][i]);
+    }
   }
 }
 
 
-void read_mux(int channel){
-  int controlPin[] = {s0, s1, s2, s3};
-
-  int muxChannel[16][4]={
-    {0,0,0,0}, //channel 0
-    {1,0,0,0}, //channel 1
-    {0,1,0,0}, //channel 2
-    {1,1,0,0}, //channel 3
-    {0,0,1,0}, //channel 4
-    {1,0,1,0}, //channel 5
-    {0,1,1,0}, //channel 6
-    {1,1,1,0}, //channel 7
-    {0,0,0,1}, //channel 8
-    {1,0,0,1}, //channel 9
-    {0,1,0,1}, //channel 10
-    {1,1,0,1}, //channel 11
-    {0,0,1,1}, //channel 12
-    {1,0,1,1}, //channel 13
-    {0,1,1,1}, //channel 14
-    {1,1,1,1}  //channel 15
-  };
-
-  //loop through the 4 sig
-  for(int i = 0; i < 4; i ++){
-    digitalWrite(controlPin[i], muxChannel[channel][i]);
+void fade_pwm_led(int led) {
+  for (int fadeValue = min_fade_intensity ; fadeValue <= max_fade_intensity; fadeValue += fade_intensity_decrement_step) {
+    // sets the value (range from 0 to 255):
+    analogWrite(led, fadeValue);
+    // wait for n milliseconds to see the dimming effect
+    delay(dimming_step);
   }
-
-
-
-  //read the value at the SIG pin
-//  int val = analogRead(SIG_pin);
-
-  //return the value
-//  return val;
+  delay (lighting_interval);
+  for (int fadeValue = max_fade_intensity ; fadeValue >= min_fade_intensity; fadeValue -= fade_intensity_decrement_step) {
+    // sets the value (range from 0 to 255):
+    //    analogWrite(10, fadeValue);
+    analogWrite(led, fadeValue);
+    // wait for n milliseconds to see the dimming effect
+    delay(dimming_step);
+  }
+  delay (fades_interval);
+  //  matrix_aux_clear_leds();
 }
+
+
 
